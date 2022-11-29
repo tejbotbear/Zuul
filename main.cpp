cf5a25f3949a54f3a772ed0090c1e6fcb1294422
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-11-29 12:15:55 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/*
+Tej Hiremath
+-
+-
+*/
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+  vector <Room*> RoomVect;
+
+  Room* OneTwenty = new Room();
+  RoomVect.push_back(OneTwenty);
+
+  Room* ScienceLab = new Room();
+  RoomVect.push_back(ScienceLab);
+
+  Room* LowerCommons = new Room();
+  RoomVect.push_back(LowerCommons);
+
+  Room* NorthHall = new Room();
+  RoomVect.push_back(NorthHall);
+
+  Room* WestHall = new Room();
+  RoomVect.push_back(WestHall)
+
+  Room* EastHall = new Room();
+  RoomVect.push_back(EastHall);
+
+  Room* SouthHall = new Room();
+  RoomVect.push_back(SouthHall);
+
+  cout << "hi" << endl;
+
+  
+
+}
