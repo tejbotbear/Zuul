d75b4c38c0edea0148b1beb7859cd5bd41397858
Tej <462750@bsd48.org>
2022-12-09 13:45:36 -0800

    push

--- items.h
+++ items.h
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Items {
 
+  bool Food = false;
+  bool Pencil = false;
+  bool Paper = false;
+  bool HallPass = false;
+  bool Binder = false;
+  
 };
--- main.cpp
+++ main.cpp
@@ -13,35 +13,78 @@ Tej Hiremath
 using namespace std;
 
 void CreateRooms(vector <Rooms*> &RoomVect);
-void Game (vector <Rooms*> &Roomvect, map <char[], char[]> Exits);
+void Game(vector <Rooms*> &Roomvect);
 
 int main() {
   vector <Rooms*> RoomVect;
-  map<int, string> Exits;
   CreateRooms(RoomVect);
 }
 
-void CreateRooms(vector <Rooms*> &RoomVect, map <char[], char[]> &Exits) {
+void CreateRooms(vector <Rooms*> &RoomVect) {
 
+  int ID = 0;
+  
+  Rooms* Inventory = new Rooms();
+  RoomVect.push_back(Inventory);
+  
   Rooms* OneTwenty = new Rooms();
+  ID = 1;
   OneTwenty->name = "OneTwenty";
   OneTwenty->description = "You are currently in the OneTwenty lab at Sunset";
   RoomVect.push_back(OneTwenty);
-  //Exits
+  
 
-
-  /*Rooms* OneHall = new Rooms();
+  Rooms* OneHall = new Rooms();
+  ID = 2;
   OneHall->name = "One Hall";
-  OneHall->description = "One hall description holder";
-  //Exits holder if needed from map
+  OneHall->description = "It seems like you can go anywhere from here.";
   RoomVect.push_back(OneHall);
-  */
-  Game(RoomVect, Exits);
+  // Create prompt to grab hall pass.
+
+  // HallPass = true;
+
+  Rooms* Cafeteria = new Rooms();
+  ID = 3;
+  Cafeteria->name = "Cafeteria";
+  Cafeteria->description = "Theres so many food options here!";
+  RoomVect.push_back(Cafeteria);
+  //Food = true;
+
+  Rooms* LowerCommons = new Rooms();
+  ID = 4;
+  LowerCommons->name = "Lower Commons";
+  LowerCommons->description = "There is open seating here.";
+  RoomVect.push_back(LowerCommons);
+
+  Rooms* MainHall = new Rooms();
+  ID = 5;
+  MainHall->name = "Main Hall";
+  MainHall->description = "There are quite a few offices nearby.";
+  RoomVect.push_back(MainHall);
+
+  Rooms* MainOffice = new Rooms();
+  ID = 6;
+  MainOffice->name = "Main Office";
+  MainOffice->description = "why are you in here? Are you in trouble?";
+  RoomVect.push_back(MainOffice);
+  //"assign pencil item"
+  //Pencil = true;
+
+  Rooms* SchoolExit = new Rooms();
+  SchoolExit->name = "School Exit";
+  SchoolExit->description = "You have left the school.";
+  RoomVect.push_back(SchoolExit);
+  
+  Game(RoomVect);
 }
 
-void Game(vector <Rooms*> &RoomVect, map <char[], char[]> &Exits) {
+void Game(vector <Rooms*> &RoomVect) {
   char Input[100];
-
-  cout << OneTwenty->description;
-  
+  int i = 0;
+  int s;
+  s = RoomVect.size();
+  for (int i = 0; i < s, i++;) {
+    Rooms* Search = RoomVect[i];
+    cout << Search->description << endl;
+  }
 }
